b6fcce83e7e9221a5387a3fb641347dc5912e1f6
Hodge <qusayhe@gmail.com>
2017-08-21 00:35:24 +0800

    change project structure

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from .models import Category
+from .models import *
 
 # Create your views here.
 
 
 def index(request):
-    categories = Category.objects.get(id=1)
-    return render(request, 'blog/index.html', context={'categories': categories})
+    categories = Category.objects.all()
+    articles = Article.objects.all()
+    tags = Tag.objects.all()
+    new_articles = Article.objects.order_by('-created_time').all()[:5]
+
+
+
+    return render(request, 'blog/index.html', context={
+                                                        'newArticles': new_articles,
+                                                        'tags': tags,
+                                                        'articles': articles,
+                                                        'categories': categories})
